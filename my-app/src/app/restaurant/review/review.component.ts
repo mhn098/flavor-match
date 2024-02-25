c532c93a4597d7a49378b984f7d5a8e291f57ef3
Nadine Hughes <nadie@unc.edu>
2024-02-24 22:13:37 -0500

    csv added

--- my-app/src/app/restaurant/review/review.component.ts
+++ my-app/src/app/restaurant/review/review.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RestaurantService } from '../restaurant.service';
+import { restaurant_model } from "../restaurant_model";
 
 @Component({
   selector: 'app-review',
@@ -8,11 +10,20 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
-export class ReviewComponent {
+export class ReviewComponent implements OnInit{
   public static Route = {
     path: 'restaurant/review',
     title: 'Review',
     component: ReviewComponent,
     canActivate: []
   };
+
+  myData: restaurant_model[] = [];
+
+  constructor(private csvService: RestaurantService) {}
+
+  ngOnInit(): void {
+    this.myData = this.csvService.getMyData();
+    console.log(this.myData);
+  }
 }
